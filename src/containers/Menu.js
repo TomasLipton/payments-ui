e117566f48318c79a2f1a62b833d05498a356c5d
Vova Yakovenko <Uladzimir.Yakavenka@itechart-group.com>
2019-10-30 19:27:38 +0300

    wip

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -1,13 +1,36 @@
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import * as cartActions from '../actions/cart';
 import uniqBy from 'lodash/uniqBy';
 import MenuComponent from "../components/Menu/Menu";
 
-const mapStateToProps = ({ cart }) => ({
+const formatItems = (cart, books) => {
+    let items;
+
+    items = uniqBy(cart.items, o => o.id);
+
+    console.log(items)
+
+    // console.log(items.map((i, index, items) => {
+    //     return {
+    //         ...items,
+    //         test: 123
+    //     }
+    //
+    //         books.items.reduce(
+    //             (count, book) =>
+    //             , 0
+    //         )
+    //     }
+    // ));
+
+    return items;
+};
+
+const mapStateToProps = ({cart, books}) => ({
     totalPrice: cart.items.reduce((total, book) => total + book.price, 0),
     count: cart.items.length,
-    items: uniqBy(cart.items, o => o.id),
+    items: formatItems(cart, books),
 });
 
 const mapDispatchToProps = dispatch => ({
